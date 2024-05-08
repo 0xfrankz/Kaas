7ab4b0659f22a35c77e0fadb9fac64878216fc74
Frank Zhang <frank.zhang@ishowdata.com>
2024-05-08 14:43:32 +0800

    refactor(backend): use lucide icon for side menu items

--- src/components/SideNavMenu.tsx
+++ src/components/SideNavMenu.tsx
@@ -1,4 +1,4 @@
-import { ArchiveIcon, FileTextIcon, GearIcon } from '@radix-ui/react-icons';
+import { MessageSquare, Package, Puzzle, Settings } from 'lucide-react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link, useLocation } from 'react-router-dom';
@@ -6,7 +6,6 @@ import { Link, useLocation } from 'react-router-dom';
 import { cn } from '@/lib/utils';
 
 import { Button } from './ui/button';
-import { ModelIcon } from './ui/icons/ModelIcon';
 
 type MenuProps = {
   expanded: boolean;
@@ -32,7 +31,7 @@ function SideNavMenuItem({
   return (
     <Button
       className={cn(
-        'flex text-base font-bold rounded-2xl mb-4 h-12 cursor-pointer items-start justify-start shadow-none hover:bg-[--gray-a4] active:bg-[--gray-a5] text-[--gray-11] hover:text-[--gray-11] transition-all overflow-hidden',
+        'flex text-base font-bold rounded-2xl mb-4 h-12 cursor-pointer items-start justify-start shadow-none hover:bg-[--gray-a4] active:bg-[--gray-a5] text-foreground transition-all overflow-hidden',
         expanded ? 'w-72 ml-4' : 'w-12 ml-2',
         active ? 'bg-[--gray-a3]' : 'bg-transparent',
         extraClassName
@@ -61,28 +60,28 @@ export function SideNavMenu({ expanded = false }: MenuProps) {
     >
       <SideNavMenuItem
         text={t('nav.conversations')}
-        icon={<ArchiveIcon className="size-4 text-foreground" />}
+        icon={<MessageSquare className="size-4 text-foreground" />}
         expanded={expanded}
         active={pathname === '/conversations'}
         to="/conversations"
       />
       <SideNavMenuItem
         text={t('nav.prompts')}
-        icon={<FileTextIcon className="size-4 text-foreground" />}
+        icon={<Puzzle className="size-4 text-foreground" />}
         expanded={expanded}
         active={pathname === '/prompts'}
         to="/prompts"
       />
       <SideNavMenuItem
         text={t('nav.models')}
-        icon={<ModelIcon className="stroke-foreground stroke-1" />}
+        icon={<Package className="size-4 text-foreground" />}
         expanded={expanded}
         active={pathname === '/models'}
         to="/models"
       />
       <SideNavMenuItem
         text={t('nav.settings')}
-        icon={<GearIcon className="size-4 text-foreground" />}
+        icon={<Settings className="size-4 text-foreground" />}
         expanded={expanded}
         active={pathname === '/settings'}
         className="mt-auto"
