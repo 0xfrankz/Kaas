64685e0755b9667ec72ffe62b49b912db73f1307
Frank Zhang <frank.zhang@ishowdata.com>
2024-07-04 18:21:59 +0800

    feat(Conversation): added button to start new conversation

--- src/components/ConversationHistory.tsx
+++ src/components/ConversationHistory.tsx
@@ -6,6 +6,7 @@ import type { ConversationDetails } from '@/lib/types';
 import { cn } from '@/lib/utils';
 
 import { Button } from './ui/button';
+import { ScrollArea } from './ui/scroll-area';
 
 type Props = {
   activeConversationId: number;
@@ -41,22 +42,25 @@ function ConversationHistoryItem({ conversation, active }: ItemProps) {
 export function ConversationHistory({ activeConversationId }: Props) {
   const { conversations } = useConversationsContext();
   return (
-    <div className="box-border h-full w-72 border-r border-border bg-background px-5 py-6">
+    <div className="box-border flex h-full max-h-screen w-72 flex-col gap-6 border-r border-border bg-background px-5 py-6">
       <h3 className="px-2 text-xs font-medium tracking-wide text-gray-400">
         Conversation history
       </h3>
-      <ul className="mt-3 flex flex-col gap-2">
-        {conversations.map((c) => {
-          return (
-            <li key={c.id} className="h-12">
-              <ConversationHistoryItem
-                conversation={c}
-                active={c.id === activeConversationId}
-              />
-            </li>
-          );
-        })}
-      </ul>
+      <ScrollArea>
+        <ul className="mt-3 flex flex-col gap-2">
+          {conversations.map((c) => {
+            return (
+              <li key={c.id} className="h-12">
+                <ConversationHistoryItem
+                  conversation={c}
+                  active={c.id === activeConversationId}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </ScrollArea>
+      <Button>New</Button>
     </div>
   );
 }
