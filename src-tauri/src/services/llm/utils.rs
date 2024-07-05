a44d25a5a727495e3f01edcec8c5f698adbafef7
Frank Zhang <frank.zhang@ishowdata.com>
2024-07-05 17:50:54 +0800

    feat: added logic to use system locale when language setting is not set (usually when newly installed)

--- src-tauri/src/services/llm/utils.rs
+++ src-tauri/src/services/llm/utils.rs
@@ -125,3 +125,28 @@ pub fn is_stream_enabled(options: &ProviderOptions) -> bool {
     }
 }
 
+// An utility function that converts string of format BCP-47 with region subtag to format BCP-47 with script subtag
+pub fn convert_locale_region_to_script(bcp47: &str) -> String {
+    let mut bcp47 = bcp47.to_string();
+    if bcp47.contains("-") {
+        let parts: Vec<&str> = bcp47.split("-").collect();
+        if parts.len() == 2 {
+            let lang = parts[0].to_string();
+            let region = parts[1].to_uppercase();
+            if lang == "zh" {
+                if region == "CN" || region == "SG" || region == "MY" {
+                    // Simplified Chinese
+                    bcp47 = format!("{}-Hans", lang);
+                } else {
+                    // Traditional Chinese
+                    bcp47 = format!("{}-Hant", lang);
+                }
+            } else if lang == "en" {
+                // use en for all English regions
+                bcp47 = lang
+            }
+        }
+    }
+    bcp47
+}
+
