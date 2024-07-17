cfe466c76b11f424c05f99594ee04552526cbbaa
Frank Zhang <frank.zhang@ishowdata.com>
2024-07-17 18:11:45 +0800

    refactor(FE): added logic to capture common keyboard shortcuts of browsers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import '@/i18n';
 
+import { useKeyPress } from 'ahooks';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from 'next-themes';
 import React, { Suspense } from 'react';
@@ -105,8 +106,106 @@ const router = createBrowserRouter([
   },
 ]);
 
+const KeyboardBlocker = () => {
+  const filter = [
+    'f1',
+    'f2',
+    'f3',
+    'f4',
+    'f5',
+    'f6',
+    'f7',
+    'f8',
+    'f9',
+    'f10',
+    'f11',
+    'f12',
+    'ctrl.a',
+    'ctrl.b',
+    'ctrl.c',
+    'ctrl.d',
+    'ctrl.e',
+    'ctrl.f',
+    'ctrl.g',
+    'ctrl.h',
+    'ctrl.i',
+    'ctrl.j',
+    'ctrl.k',
+    'ctrl.l',
+    'ctrl.m',
+    'ctrl.n',
+    'ctrl.o',
+    'ctrl.p',
+    'ctrl.q',
+    'ctrl.r',
+    'ctrl.s',
+    'ctrl.t',
+    'ctrl.u',
+    'ctrl.v',
+    'ctrl.w',
+    'ctrl.x',
+    'ctrl.y',
+    'ctrl.z',
+    'ctrl.0',
+    'ctrl.1',
+    'ctrl.2',
+    'ctrl.3',
+    'ctrl.4',
+    'ctrl.5',
+    'ctrl.6',
+    'ctrl.7',
+    'ctrl.8',
+    'ctrl.9',
+    'ctrl.graveaccent',
+    'ctrl.dash',
+    'ctrl.equalsign',
+    'ctrl.openbracket',
+    'ctrl.closebracket',
+    'ctrl.backslash',
+    'ctrl.semicolon',
+    'ctrl.singlequote',
+    'ctrl.comma',
+    'ctrl.period',
+    'ctrl.forwardslash',
+  ];
+  useKeyPress(
+    filter,
+    (e, key) => {
+      switch (key) {
+        case 'ctrl.a':
+          // ctrl+a is allowed on input and textarea
+          if (e.target instanceof Element) {
+            const tagName = e.target?.tagName.toLowerCase();
+            if (tagName !== 'input' && tagName !== 'textarea') {
+              e.preventDefault();
+              e.stopPropagation();
+            }
+          }
+          break;
+        case 'ctrl.c':
+          // ctrl+c is allowed
+          break;
+        case 'ctrl.v':
+          // ctrl+v is allowed
+          break;
+        case 'ctrl.x':
+          // ctrl+x is allowed
+          break;
+        default:
+          e.preventDefault();
+          e.stopPropagation();
+      }
+    },
+    {
+      exactMatch: true,
+    }
+  );
+  return null;
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <KeyboardBlocker />
     {/* Wrap everything in an empty context menu to avoid native */}
     {/* context menu from showing up when right clicking on the page */}
     <ContextMenu>
