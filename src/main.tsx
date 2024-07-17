43c48777a10e59e76555a96987b0b824faeb3a58
Frank Zhang <frank.zhang@ishowdata.com>
2024-07-17 12:11:42 +0800

    refactor(FE): added a global context menu to hide native menu when right clicking

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import {
   useOutlet,
 } from 'react-router-dom';
 
+import logoImg from '@/assets/images/logo.svg';
 import CommonLayout from '@/layouts/CommonLayout';
 import {
   ConversationsContextProvider,
@@ -30,6 +31,11 @@ import SettingsPage from '@/pages/Settings';
 
 import { GlobalFallback } from './components/GlobalFallback';
 import { PageSkeleton } from './components/placeholders/WholePage';
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuTrigger,
+} from './components/ui/context-menu';
 import { TooltipProvider } from './components/ui/tooltip';
 import Dependencies from './pages/Dependencies';
 
@@ -101,18 +107,29 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary FallbackComponent={GlobalFallback}>
-      <RQProvider>
-        <ThemeProvider defaultTheme="system" attribute="class">
-          <TooltipProvider delayDuration={0}>
-            <Suspense fallback={<PageSkeleton />}>
-              <InitializationProvider>
-                <RouterProvider router={router} />
-              </InitializationProvider>
-            </Suspense>
-          </TooltipProvider>
-        </ThemeProvider>
-      </RQProvider>
-    </ErrorBoundary>
+    {/* Wrap everything in an empty context menu to avoid native */}
+    {/* context menu from showing up when right clicking on the page */}
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <ErrorBoundary FallbackComponent={GlobalFallback}>
+          <RQProvider>
+            <ThemeProvider defaultTheme="system" attribute="class">
+              <TooltipProvider delayDuration={0}>
+                <Suspense fallback={<PageSkeleton />}>
+                  <InitializationProvider>
+                    <RouterProvider router={router} />
+                  </InitializationProvider>
+                </Suspense>
+              </TooltipProvider>
+            </ThemeProvider>
+          </RQProvider>
+        </ErrorBoundary>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="min-w-fit">
+        <div className="p-1">
+          <img src={logoImg} alt="Kaas" width={16} height={16} />
+        </div>
+      </ContextMenuContent>
+    </ContextMenu>
   </React.StrictMode>
 );
