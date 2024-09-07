3e9a8d924c328ce2048d3bafc8b7316eb5844bec
Frank Zhang <1282608+0xfrankz@users.noreply.github.com>
2024-09-07 18:19:20 +0800

    feat(Conversation): added support to chat with multiple models, wip-2

--- src/components/ModelPickerDialog.tsx
+++ src/components/ModelPickerDialog.tsx
@@ -5,9 +5,11 @@ import { useAppStateStore } from '@/lib/store';
 import type { Model, StatefulDialogHandler } from '@/lib/types';
 import { getModelAlias } from '@/lib/utils';
 
+import { MultiModelPicker } from './MultiModelPicker';
 import { ProviderIcon } from './ProviderIcon';
 import { Button } from './ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from './ui/dialog';
+import { Label } from './ui/label';
 import {
   Select,
   SelectContent,
@@ -15,6 +17,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from './ui/select';
+import { Switch } from './ui/switch';
 
 type DialogProps = {
   onSubmit: (model: Model) => void;
@@ -25,8 +28,9 @@ export const ModelPickerDialog = forwardRef<
   DialogProps
 >(({ onSubmit }, ref) => {
   const [showDialog, setShowDialog] = useState(false);
-  const isOpenRef = useRef(false);
+  const [useMultipleModels, setUseMultipleModels] = useState(false);
   const [subject, setSubject] = useState('');
+  const isOpenRef = useRef(false);
   const { t } = useTranslation(['page-conversation']);
   const { models } = useAppStateStore();
   const defaultModel = models[0];
@@ -58,30 +62,38 @@ export const ModelPickerDialog = forwardRef<
             {t('page-conversation:section:choose-model', { subject })}
           </DialogTitle>
         </DialogHeader>
-        <div className="flex gap-4">
-          <Select
-            onValueChange={(v) => {
-              selectedModelRef.current = parseInt(v, 10);
-            }}
-            defaultValue={defaultModel.id.toString()}
-          >
-            <SelectTrigger>
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              {models.map((model) => (
-                <SelectItem value={model.id.toString()} key={model.id}>
-                  <div className="flex gap-2">
-                    <ProviderIcon provider={model.provider} />
-                    {getModelAlias(model)}
-                  </div>
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Button onClick={onClick}>
-            {t('generic:action:use-this-model')}
-          </Button>
+        {useMultipleModels ? (
+          <MultiModelPicker models={models} />
+        ) : (
+          <div className="flex gap-4">
+            <Select
+              onValueChange={(v) => {
+                selectedModelRef.current = parseInt(v, 10);
+              }}
+              defaultValue={defaultModel.id.toString()}
+            >
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                {models.map((model) => (
+                  <SelectItem value={model.id.toString()} key={model.id}>
+                    <div className="flex gap-2">
+                      <ProviderIcon provider={model.provider} />
+                      {getModelAlias(model)}
+                    </div>
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Button onClick={onClick}>
+              {t('generic:action:use-this-model')}
+            </Button>
+          </div>
+        )}
+        <div className="flex items-center gap-4">
+          <Switch id="multiple-models" onCheckedChange={setUseMultipleModels} />
+          <Label htmlFor="multiple-models">Use multiple models</Label>
         </div>
       </DialogContent>
     </Dialog>
